76546d6b7adca4d20bb248ca33579e29eba9a13a
Hugh Avery <hughavery101@gmail.com>
2025-10-08 12:53:24 +1100

    todo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -139,8 +139,9 @@ const Hero = () => {
           </div>
         </div>
       </div>
-
-      {/* Scroll indicator */}
+      
+      {/* todo make the mouse Mobile frendly  */}
+      {/* Scroll indicator */}  
       <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
         <div className="w-6 h-10 border-2 border-gray-400 rounded-full flex justify-center">
           <div className="w-1 h-3 bg-gray-400 rounded-full mt-2 animate-pulse"></div>
